c6f3d35daaefa61ea83da702effd3260e1aacd02
Cerisedw <CarnetsDeCerise@gmail.com>
2019-06-21 12:03:46 +0200

    Add files via upload

---
+++ Suite_exercices/Anagrame/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string rejouer = "Y";
+
+            while (rejouer.ToUpper() == "Y")
+            {
+                bool recommence = false;
+
+
+                string mot = TakeWord();
+
+                //Console.WriteLine(mot);
+
+                char[] m_char = mot.ToCharArray();
+
+                string rand_word = RandomWord(m_char);
+
+                while (rand_word == mot)
+                {
+                    rand_word = RandomWord(m_char);
+                }
+
+                while (!recommence)
+                {
+                    Console.WriteLine("Devinez le mot de base : {0}", rand_word);
+                    string reponse = Console.ReadLine();
+
+                    if (reponse.ToUpper() == mot)
+                    {
+                        Console.WriteLine("Gagné");
+                        recommence = true;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Perdu");
+                    }
+                }
+
+                Console.WriteLine("Voulez-vous rejouer ? Y/N");
+                rejouer = Console.ReadLine();
+            }
+            Console.WriteLine("Merci d'avoir joué !");
+
+            Console.ReadLine();
+
+        }
+
+        public static string TakeWord()
+        {
+            Random random = new Random();
+
+            StreamReader file = new StreamReader("mots.txt");
+
+
+            string mot = "";
+         
+            int rand = random.Next(1, 800);
+
+            for (int j = 0; j < rand; j++)
+            {
+                mot = file.ReadLine();
+            }
+            return mot;
+        }
+
+        public static string RandomWord(char[] arr_char)
+        {
+            Random random = new Random();
+
+            string final_word = "";
+
+            int longueur = arr_char.Count();
+            List<int> temp = new List<int>();
+
+            for (int i = 0; i < longueur; i++)
+            {
+                if (i == 0)
+                {
+                    int r = random.Next(0, longueur);
+                    final_word += arr_char[r];
+                    temp.Add(r);
+                }
+                else
+                {
+                    int r = random.Next(0, longueur);
+                    while (temp.Contains(r))
+                    {
+                        r = random.Next(0, longueur);
+                    }
+                    final_word += arr_char[r];
+                    temp.Add(r);
+                }
+            }
+            return final_word;
+        }
+
+    }
+}
---
+++ Suite_exercices/Suite_exercices/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suite_exercices
+{
+    class Program
+    {
+        public enum JourSemaine { Lundi, Mardi, Mercredi, Jeudi, Vendredi, Samedi, Dimanche }
+
+
+        static void Main(string[] args)
+        {
+
+            #region ex methode ref
+            //string phrase = MaConcat("Comment", "ça", "va ?");
+
+            //Console.WriteLine(phrase);
+
+
+            //double a = 2.5;
+            //Console.WriteLine("Avant : {0}", a);
+
+            //MethodeRef(a);
+            //Console.WriteLine("Après : {0}", a);
+
+            //MethodeRef2(ref a);
+            //Console.WriteLine("Après : {0}", a);
+            #endregion
+
+            #region equation ex structure
+
+            //Equation equa;
+
+            //double? X1, X2;
+
+            //equa.A = 2.2;
+            //equa.B = 13;
+            //equa.C = 0;
+
+            //bool b = equa.Resoudre(out X1, out X2);
+
+            //string end = (b) ? "Une réponse a été trouvée." : "Une réponse n'a pas été trouvée.";
+
+            //if (b)
+            //{
+            //    Console.WriteLine(end);
+            //    Console.WriteLine("X1 = {0} | X2 = {1}", X1, X2);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine(end);
+            //    Console.WriteLine("X1 = null | X2 = null");
+
+            //}
+
+            #endregion
+
+
+            //foreach (string s in Enum.GetNames(typeof(JourSemaine)))
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            //JourSemaine jour_sem = JourSemaine.Lundi;
+
+            //Console.WriteLine(jour_sem);
+
+            //Console.WriteLine((int)jour_sem);
+
+            //string j = "Dimanche";
+            //JourSemaine jsem;
+
+
+            //if (Enum.TryParse<JourSemaine>(j, out jsem)){
+            //    Console.WriteLine(jsem);
+            //    Console.WriteLine((int)jsem);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Error");
+            //}
+
+
+
+            #region Anagrame EX
+
+
+
+            #endregion
+
+            Console.ReadLine();
+
+        }
+
+
+        public static string MaConcat(params string[] lesMots)
+        {
+            string phrase = "";
+            foreach (string m in lesMots)
+            {
+                phrase += m + " ";
+            }
+
+            return phrase;
+        }
+
+
+        public static void MethodeRef(double nb)
+        {
+            nb += 42;
+        }
+        public static void MethodeRef2(ref double nb)
+        {
+            nb += 42;
+        }
+
+
+
+
+        public struct Equation
+        {
+            public double A, B, C;
+
+            public bool Resoudre(out double? X1, out double? X2)
+            {
+                bool retour = true;
+                double delta = (Math.Pow(B, 2)) - (4 * A * C);
+                if (delta > 0)
+                {
+                    X1 = ((-B) + Math.Sqrt(delta)) / (2 * A);
+                    X2 = ((-B) - Math.Sqrt(delta)) / (2 * A);
+                }
+                else if (delta == 0)
+                {
+                    X1 = (-B) / (2 * A);
+                    X2 = X1;
+                }
+                else
+                {
+                    X1 = null;
+                    X2 = null;
+                    retour = false;
+                }
+                return retour;
+            }
+
+        }
+
+
+    }
+}
